d52f23cae1c208f92ede8f6b9167f52dfbe3b42f
Saurabh Garg <gargsaurabh1993@gmail.com>
2014-05-03 00:24:48 +0530

    Rulers

---
+++ rulers/mughal.php
@@ -0,0 +1,19 @@
+<?php
+$link = mysql_connect('localhost','root','');
+if (!$link) {
+    die('Could not connect: ' . mysql_error());
+}
+
+$db_selected = mysql_select_db('dynasties', $link);
+if (!$db_selected) {
+    die ('Can\'t use HistoryMatters : ' . mysql_error());
+}
+$id = $_GET['id'];
+$q = "select * from mughal where id=$id LIMIT 1";
+$sql = mysql_query($q) or die(mysql_error());
+
+$row = mysql_fetch_assoc($sql);
+
+echo $row['des'];
+
+?>
